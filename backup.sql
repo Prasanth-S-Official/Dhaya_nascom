8fce19121f818b532c1b6db4794f922cce1000a7
neojarvis <techadmin@iamneo.ai>
2024-10-24 05:29:29 +0000

    Auto-save-commit-24/10/2024--10:59:26

--- backup.sql
+++ backup.sql
@@ -22,6 +22,147 @@
 CREATE DATABASE /*!32312 IF NOT EXISTS*/ `appdb` /*!40100 DEFAULT CHARACTER SET latin1 */;
 
 USE `appdb`;
+
+--
+-- Table structure for table `feedback`
+--
+
+DROP TABLE IF EXISTS `feedback`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `feedback` (
+  `feedback_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `date` date DEFAULT NULL,
+  `feedback_text` varchar(255) DEFAULT NULL,
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`feedback_id`),
+  KEY `FK7k33yw505d347mw3avr93akao` (`user_id`),
+  CONSTRAINT `FK7k33yw505d347mw3avr93akao` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `feedback`
+--
+
+LOCK TABLES `feedback` WRITE;
+/*!40000 ALTER TABLE `feedback` DISABLE KEYS */;
+/*!40000 ALTER TABLE `feedback` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `loan`
+--
+
+DROP TABLE IF EXISTS `loan`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `loan` (
+  `loan_id` bigint(20) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `documents_required` varchar(255) DEFAULT NULL,
+  `eligibility` varchar(255) DEFAULT NULL,
+  `interest_rate` double NOT NULL,
+  `loan_type` varchar(255) DEFAULT NULL,
+  `maximum_amount` double NOT NULL,
+  `repayment_tenure` int(11) NOT NULL,
+  PRIMARY KEY (`loan_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `loan`
+--
+
+LOCK TABLES `loan` WRITE;
+/*!40000 ALTER TABLE `loan` DISABLE KEYS */;
+/*!40000 ALTER TABLE `loan` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `loan_application`
+--
+
+DROP TABLE IF EXISTS `loan_application`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `loan_application` (
+  `loan_application_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `loan_status` int(11) NOT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `course` varchar(255) DEFAULT NULL,
+  `image` longblob NOT NULL,
+  `institution` varchar(255) DEFAULT NULL,
+  `submission_date` date DEFAULT NULL,
+  `tuition_fee` double NOT NULL,
+  `loan_id` bigint(20) DEFAULT NULL,
+  `user_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`loan_application_id`),
+  KEY `FKkrn3iq83xftqmisca4n75sbml` (`loan_id`),
+  KEY `FK9mvuefv6gd4ggkfkcooi087vd` (`user_id`),
+  CONSTRAINT `FK9mvuefv6gd4ggkfkcooi087vd` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`),
+  CONSTRAINT `FKkrn3iq83xftqmisca4n75sbml` FOREIGN KEY (`loan_id`) REFERENCES `loan` (`loan_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `loan_application`
+--
+
+LOCK TABLES `loan_application` WRITE;
+/*!40000 ALTER TABLE `loan_application` DISABLE KEYS */;
+/*!40000 ALTER TABLE `loan_application` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `loan_seq`
+--
+
+DROP TABLE IF EXISTS `loan_seq`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `loan_seq` (
+  `next_val` bigint(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `loan_seq`
+--
+
+LOCK TABLES `loan_seq` WRITE;
+/*!40000 ALTER TABLE `loan_seq` DISABLE KEYS */;
+INSERT INTO `loan_seq` VALUES (1);
+/*!40000 ALTER TABLE `loan_seq` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) DEFAULT NULL,
+  `mobile_number` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `user_role` varchar(255) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'admin@gmail.com','1234567890','$2a$10$dvnXcHQNPLIHKpDiXslcaO5B5TDtmYxaoojD3eUo5lfN9cmaqgKlC','Admin','admin');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -32,4 +173,4 @@ USE `appdb`;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-10-23 11:43:17
+-- Dump completed on 2024-10-24  5:29:29
