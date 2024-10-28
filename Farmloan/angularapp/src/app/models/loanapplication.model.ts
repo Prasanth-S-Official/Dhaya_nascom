f084cee2c56ade534cdd37a631cc817d77f17ac5
neojarvis <techadmin@iamneo.ai>
2024-10-28 16:45:23 +0000

    Auto-save-commit-28/10/2024--22:15:20

--- Farmloan/angularapp/src/app/models/loanapplication.model.ts
+++ Farmloan/angularapp/src/app/models/loanapplication.model.ts
@@ -13,14 +13,14 @@
 
 
 export interface LoanApplication {
-    loanApplicationId?: number;         // Optional ID for the loan application
-    userId?: number;                    // ID of the user applying for the loan
-    loanId?: number;                    // ID of the associated loan
-    submissionDate: string;             // Date when the loan application was submitted
+    loanApplicationId?: number;         
+    userId?: number;                   
+    loanId?: number;                    
+    submissionDate: string;             
     loanStatus: number;                 // Status of the loan (0: Pending, 1: Approved, 2: Rejected)
-    farmLocation: string;               // Location of the farm related to the loan application
-    farmerAddress: string;              // Address of the farmer
-    farmSizeInAcres: number;            // Size of the farm in acres
-    farmPurpose: string;                // Purpose of the farm
-    file: string;                       // File associated with the loan application
+    farmLocation: string;               
+    farmerAddress: string;             
+    farmSizeInAcres: number;            
+    farmPurpose: string;               
+    file: string;                       
 }
