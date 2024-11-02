8c7429fe854bf8e772a9f3881e1a9e6199dd41ab
neojarvis <techadmin@iamneo.ai>
2024-11-02 15:14:47 +0000

    Auto-save-commit-02/11/2024--20:44:45

--- physicMentor/springapp/src/main/java/com/examly/springapp/controller/PhysicalTrainingRequestController.java
+++ physicMentor/springapp/src/main/java/com/examly/springapp/controller/PhysicalTrainingRequestController.java
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/physicalTrainingRequests")
+@RequestMapping("/api/physical-training-request")
 public class PhysicalTrainingRequestController {
 
     @Autowired
