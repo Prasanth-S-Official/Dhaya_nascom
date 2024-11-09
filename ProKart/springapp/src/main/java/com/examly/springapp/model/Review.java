4d9b4eab5755fd2445920f25bd1658a0598dde42
neojarvis <techadmin@iamneo.ai>
2024-11-09 07:04:10 +0000

    Auto-save-commit-09/11/2024--12:34:05

--- ProKart/springapp/src/main/java/com/examly/springapp/model/Review.java
+++ ProKart/springapp/src/main/java/com/examly/springapp/model/Review.java
@@ -1,3 +1,75 @@
+package com.examly.springapp.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+@Entity
 public class Review {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long reviewId;
+    private String reviewText;
+    private int rating;
+    private LocalDate date;
+
+    @ManyToOne
+    @JoinColumn(name = "userId", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "productId", nullable = false)
+    private Product product;
+
+    public Review() {
+    }
+
+    // Getters and Setters
+    public Long getReviewId() {
+        return reviewId;
+    }
+
+    public void setReviewId(Long reviewId) {
+        this.reviewId = reviewId;
+    }
+
+    public String getReviewText() {
+        return reviewText;
+    }
+
+    public void setReviewText(String reviewText) {
+        this.reviewText = reviewText;
+    }
+
+    public int getRating() {
+        return rating;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Product getProduct() {
+        return product;
+    }
+
+    public void setProduct(Product product) {
+        this.product = product;
+    }
 }
