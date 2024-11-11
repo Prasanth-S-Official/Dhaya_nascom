10d235d000e1ad86b61b6e0d9df8e43c827efc02
neojarvis <techadmin@iamneo.ai>
2024-11-11 05:43:50 +0000

    Auto-save-commit-11/11/2024--11:13:43

--- ProKart/springapp/src/main/java/com/examly/springapp/service/ProductService.java
+++ ProKart/springapp/src/main/java/com/examly/springapp/service/ProductService.java
@@ -1,3 +1,13 @@
-public class ProductService {
-    
+package com.examly.springapp.service;
+
+import java.util.List;
+import java.util.Optional;
+import com.examly.springapp.model.Product;
+
+public interface ProductService {
+    Product addProduct(Product product);
+    Optional<Product> getProductById(Long productId);
+    List<Product> getAllProducts();
+    Product updateProduct(Long productId, Product product);
+    boolean deleteProduct(Long productId);
 }
---
+++ ProKart/springapp/src/main/java/com/examly/springapp/service/ProductServiceImpl.java
@@ -0,0 +1,43 @@
+package com.examly.springapp.service;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.examly.springapp.model.Product;
+import com.examly.springapp.repository.ProductRepo;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    @Autowired
+    private ProductRepo productRepo;
+
+    public Product addProduct(Product product) {
+        return productRepo.save(product);
+    }
+
+    public Optional<Product> getProductById(Long productId) {
+        return productRepo.findById(productId);
+    }
+
+    public List<Product> getAllProducts() {
+        return productRepo.findAll();
+    }
+
+    public Product updateProduct(Long productId, Product updatedProduct) {
+        if (productRepo.existsById(productId)) {
+            updatedProduct.setProductId(productId);
+            return productRepo.save(updatedProduct);
+        }
+        return null;
+    }
+
+    public boolean deleteProduct(Long productId) {
+        if (productRepo.existsById(productId)) {
+            productRepo.deleteById(productId);
+            return true;
+        }
+        return false;
+    }
+}
