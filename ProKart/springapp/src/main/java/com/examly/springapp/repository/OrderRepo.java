eda5f239fd41f0910b9957bb63f728b4aacd3a57
neojarvis <techadmin@iamneo.ai>
2024-11-11 04:43:32 +0000

    Auto-save-commit-11/11/2024--10:13:25

---
+++ ProKart/springapp/src/main/java/com/examly/springapp/repository/OrderRepo.java
@@ -0,0 +1,17 @@
+package com.examly.springapp.repository;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+import com.examly.springapp.model.Order;
+
+@Repository
+public interface OrderRepo extends JpaRepository<Order, Long> {
+
+    @Query("SELECT o FROM Order o WHERE o.user.userId = :userId")
+    List<Order> findOrdersByUserId(Long userId);
+    
+    @Query("SELECT o FROM Order o WHERE o.orderStatus = :status")
+    List<Order> findOrdersByStatus(String status);
+}
