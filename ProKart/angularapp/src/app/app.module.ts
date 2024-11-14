7bd40680f9a47bc35013ede766254280505c646f
neojarvis <techadmin@iamneo.ai>
2024-11-14 09:42:59 +0000

    Auto-save-commit-14/11/2024--15:12:58

--- ProKart/angularapp/src/app/app.module.ts
+++ ProKart/angularapp/src/app/app.module.ts
@@ -6,18 +6,7 @@ import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { LoginComponent } from './components/login/login.component';
 import { ErrorComponent } from './components/error/error.component';
 import { HttpClientModule } from '@angular/common/http';
-import { CreateloanComponent } from './components/createloan/createloan.component';
-import { RequestedloanComponent } from './components/requestedloan/requestedloan.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { ViewloanComponent } from './components/viewloan/viewloan.component';
-import { UserviewloanComponent } from './components/userviewloan/userviewloan.component';
-import { UserappliedloanComponent } from './components/userappliedloan/userappliedloan.component';
 import { CommonModule } from '@angular/common';
-import { AdmineditloanComponent } from './components/admineditloan/admineditloan.component';
-import { LoanformComponent } from './components/loanform/loanform.component';
-import { AdminviewfeedbackComponent } from './components/adminviewfeedback/adminviewfeedback.component';
-import { UserviewfeedbackComponent } from './components/userviewfeedback/userviewfeedback.component';
-import { UseraddfeedbackComponent } from './components/useraddfeedback/useraddfeedback.component';
 import { AdminnavComponent } from './components/adminnav/adminnav.component';
 import { UsernavComponent } from './components/usernav/usernav.component';
 import { SignupComponent } from './components/signup/signup.component';
@@ -40,21 +29,9 @@ import { AdminviewreviewsComponent } from './components/adminviewreviews/adminvi
   declarations: [
     AppComponent,
     LoginComponent,
-    // RegistrationComponent,
     SignupComponent,
     HomePageComponent,
     ErrorComponent,
-    CreateloanComponent,
-    NavbarComponent,
-    RequestedloanComponent,
-    ViewloanComponent,
-    UserviewloanComponent,
-    UserappliedloanComponent,
-    AdmineditloanComponent,
-    LoanformComponent,
-    AdminviewfeedbackComponent,
-    UserviewfeedbackComponent,
-    UseraddfeedbackComponent,
     AdminnavComponent,
     UsernavComponent,
     ProductCreateComponent,
