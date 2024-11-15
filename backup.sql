b58b2c190630d39bff2a8afa45048a51043e23d8
neojarvis <techadmin@iamneo.ai>
2024-11-15 08:57:56 +0000

    Auto-save-commit-15/11/2024--14:27:53

--- backup.sql
+++ backup.sql
@@ -162,7 +162,7 @@ CREATE TABLE `user` (
   `user_role` varchar(255) DEFAULT NULL,
   `username` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -171,7 +171,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'admin@gmail.com','1234567890','$2a$10$WQQupswJDcrnFHqk5CHVM.K0Z12gOGBdWcyAxMcWWMScvANJNgCyG','Admin','admin'),(2,'user@gmail.com','1234567890','$2a$10$80LrxzQhXnyFFYMxjkHOzepsxiCCufywpCdrUNCIAuDGG06yMmPeW','USER','Demo@123'),(3,'user2@gmail.com','1234567890','$2a$10$RMQUZLzPb24MceFzuOtK/./tQDmHe1qhoB1iO4z73vbwzX05twgr6','User','User');
+INSERT INTO `user` VALUES (1,'admin@gmail.com','1234567890','$2a$10$WQQupswJDcrnFHqk5CHVM.K0Z12gOGBdWcyAxMcWWMScvANJNgCyG','Admin','admin'),(2,'user@gmail.com','1234567890','$2a$10$80LrxzQhXnyFFYMxjkHOzepsxiCCufywpCdrUNCIAuDGG06yMmPeW','USER','Demo@123'),(3,'user2@gmail.com','1234567890','$2a$10$RMQUZLzPb24MceFzuOtK/./tQDmHe1qhoB1iO4z73vbwzX05twgr6','User','User'),(4,'demouser2@gmail.com','7373297990','$2a$10$l.CM/op16rqtVpggQaNKuuQvCD8VUsbVDWw2WPvfaoyNPW.GaBBqe','User','demo');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -184,4 +184,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-11-15  8:42:52
+-- Dump completed on 2024-11-15  8:57:55
