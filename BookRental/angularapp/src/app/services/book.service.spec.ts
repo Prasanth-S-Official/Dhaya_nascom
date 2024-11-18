7e07c8390defb36929914c881e6f92f1c2220423
neojarvis <techadmin@iamneo.ai>
2024-11-18 15:43:37 +0000

    Auto-save-commit-18/11/2024--21:13:37

--- BookRental/angularapp/src/app/services/book.service.spec.ts
+++ BookRental/angularapp/src/app/services/book.service.spec.ts
@@ -1,16 +1,19 @@
 import { TestBed } from '@angular/core/testing';
 
 import { BookService } from './book.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('BookService', () => {
   let service: BookService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(BookService);
   });
 
-  it('should be created', () => {
+  fit('Frontend_should_create_book_service', () => {
     expect(service).toBeTruthy();
   });
 });
--- BookRental/angularapp/src/app/services/bookrentalrequest.service.spec.ts
+++ BookRental/angularapp/src/app/services/bookrentalrequest.service.spec.ts
@@ -1,16 +1,19 @@
 import { TestBed } from '@angular/core/testing';
 
 import { BookrentalrequestService } from './bookrentalrequest.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('BookrentalrequestService', () => {
   let service: BookrentalrequestService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(BookrentalrequestService);
   });
 
-  it('should be created', () => {
+  fit('Frontend_should_create_bookrentalrequest_service', () => {
     expect(service).toBeTruthy();
   });
 });
