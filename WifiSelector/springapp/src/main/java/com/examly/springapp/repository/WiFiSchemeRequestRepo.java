0e2c957ed6cf60eee797e4e0a2a0489b2de5a1f6
neojarvis <techadmin@iamneo.ai>
2024-11-21 04:56:25 +0000

    Auto-save-commit-21/11/2024--10:26:24

--- WifiSelector/springapp/src/main/java/com/examly/springapp/repository/WiFiSchemeRequestRepo.java
+++ WifiSelector/springapp/src/main/java/com/examly/springapp/repository/WiFiSchemeRequestRepo.java
@@ -1,3 +1,23 @@
-public class WiFiSchemeRequestRepo {
-    
+package com.examly.springapp.repository;
+
+import com.examly.springapp.model.WiFiSchemeRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface WiFiSchemeRequestRepo extends JpaRepository<WiFiSchemeRequest, Long> {
+
+    @Query("SELECT w FROM WiFiSchemeRequest w WHERE w.user.userId = :userId")
+    List<WiFiSchemeRequest> findWiFiSchemeRequestsByUserId(long userId);
+
+    @Query("SELECT w FROM WiFiSchemeRequest w WHERE w.wifiScheme.wifiSchemeId = :wifiSchemeId")
+    List<WiFiSchemeRequest> findWiFiSchemeRequestsBySchemeId(Long wifiSchemeId);
+
+    Optional<WiFiSchemeRequest> findByUser_UserIdAndWiFiScheme_WifiSchemeId(long userId, Long wifiSchemeId);
+
+    boolean existsByWiFiScheme_WifiSchemeId(Long wifiSchemeId);
 }
