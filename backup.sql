77e3763c3ebe6f58b1b8e2a1c790cebc079fbe5f
neojarvis <techadmin@iamneo.ai>
2024-12-03 18:22:33 +0000

    Auto-save-commit-03/12/2024--23:52:32

--- backup.sql
+++ backup.sql
@@ -42,7 +42,7 @@ CREATE TABLE `feedback` (
   KEY `FK7k33yw505d347mw3avr93akao` (`user_id`),
   CONSTRAINT `FK5l9cw67rm0js1fopkx5yxcihr` FOREIGN KEY (`material_id`) REFERENCES `material` (`material_id`),
   CONSTRAINT `FK7k33yw505d347mw3avr93akao` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -51,7 +51,7 @@ CREATE TABLE `feedback` (
 
 LOCK TABLES `feedback` WRITE;
 /*!40000 ALTER TABLE `feedback` DISABLE KEYS */;
-INSERT INTO `feedback` VALUES (1,'Pricing','2024-12-03','demo feedback over price',1,2);
+INSERT INTO `feedback` VALUES (1,'Pricing','2024-12-03','demo feedback over price',1,2),(2,'Quality','2024-12-03','Excellent!!!',3,2);
 /*!40000 ALTER TABLE `feedback` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -159,4 +159,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-12-03 18:07:33
+-- Dump completed on 2024-12-03 18:22:33
