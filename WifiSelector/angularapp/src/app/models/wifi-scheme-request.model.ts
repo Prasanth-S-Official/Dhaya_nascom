d6deb170a7130a76b425d8ec30f6fa2c50591024
neojarvis <techadmin@iamneo.ai>
2024-12-11 08:15:01 +0000

    Auto-save-commit-11/12/2024--13:44:59

--- WifiSelector/angularapp/src/app/models/wifi-scheme-request.model.ts
+++ WifiSelector/angularapp/src/app/models/wifi-scheme-request.model.ts
@@ -2,9 +2,9 @@ export interface WifiSchemeRequest {
     wifiSchemeRequestId?: number;   // Unique identifier for the request
     userId: number;                 // ID of the user making the request
     wifiSchemeId: number;           // ID of the WiFi scheme being requested
-    requestDate: string;            // Date of the request (use ISO format: YYYY-MM-DD)
+    requestDate: Date;            // Date of the request (use ISO format: YYYY-MM-DD)
     status: string;                 // E.g., Pending, Approved, Rejected
-    comments?: string;              // Optional: Additional comments or requests from the user
+    comments: string;              // Optional: Additional comments or requests from the user
     proof: string;                  // Proof document (base64 encoded)
 
     // Address fields
