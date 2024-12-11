9713e87201639c60a0ff5fe5500f1e90f74a74ef
neojarvis <techadmin@iamneo.ai>
2024-12-11 10:30:55 +0000

    Auto-save-commit-11/12/2024--16:00:53

--- WifiSelector/springapp/src/main/java/com/examly/springapp/service/WiFiSchemeRequestService.java
+++ WifiSelector/springapp/src/main/java/com/examly/springapp/service/WiFiSchemeRequestService.java
@@ -9,7 +9,7 @@ public interface WiFiSchemeRequestService {
     WiFiSchemeRequest addWiFiSchemeRequest(WiFiSchemeRequest request);
     Optional<WiFiSchemeRequest> getWiFiSchemeRequestById(Long wifiSchemeRequestId); 
     List<WiFiSchemeRequest> getAllWiFiSchemeRequests();
-    WiFiSchemeRequest updateWiFiSchemeRequest(Long wifiSchemeRequestId, WiFiSchemeRequest request); ]
+    WiFiSchemeRequest updateWiFiSchemeRequest(Long wifiSchemeRequestId, WiFiSchemeRequest request); 
     boolean deleteWiFiSchemeRequest(Long wifiSchemeRequestId);
     List<WiFiSchemeRequest> getWiFiSchemeRequestsByUserId(Long userId);
 }
