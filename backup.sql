4e147e1afb1fd79d6f55d8680fbdc6a187075cf7
neojarvis <techadmin@iamneo.ai>
2024-12-12 08:47:48 +0000

    Auto-save-commit-12/12/2024--14:17:45

--- backup.sql
+++ backup.sql
@@ -37,11 +37,14 @@ CREATE TABLE `feedback` (
   `feedback_text` varchar(255) DEFAULT NULL,
   `user_id` bigint(20) NOT NULL,
   `wifi_scheme_id` bigint(20) DEFAULT NULL,
+  `trainer_id` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`feedback_id`),
   KEY `FK7k33yw505d347mw3avr93akao` (`user_id`),
   KEY `FK1jag4aj6c4bvpvluq5kbaci1f` (`wifi_scheme_id`),
+  KEY `FKbemwsc34h9o14qmv3mnnkesuv` (`trainer_id`),
   CONSTRAINT `FK1jag4aj6c4bvpvluq5kbaci1f` FOREIGN KEY (`wifi_scheme_id`) REFERENCES `wi_fi_scheme` (`wifi_scheme_id`),
-  CONSTRAINT `FK7k33yw505d347mw3avr93akao` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
+  CONSTRAINT `FK7k33yw505d347mw3avr93akao` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`),
+  CONSTRAINT `FKbemwsc34h9o14qmv3mnnkesuv` FOREIGN KEY (`trainer_id`) REFERENCES `trainer` (`trainer_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -52,6 +55,73 @@ CREATE TABLE `feedback` (
 LOCK TABLES `feedback` WRITE;
 /*!40000 ALTER TABLE `feedback` DISABLE KEYS */;
 /*!40000 ALTER TABLE `feedback` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `requirement`
+--
+
+DROP TABLE IF EXISTS `requirement`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `requirement` (
+  `requirement_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `budget` double DEFAULT NULL,
+  `department` varchar(255) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `duration` varchar(255) DEFAULT NULL,
+  `location` varchar(255) DEFAULT NULL,
+  `mode` varchar(255) DEFAULT NULL,
+  `posted_date` date DEFAULT NULL,
+  `priority` varchar(255) DEFAULT NULL,
+  `skill_level` varchar(255) DEFAULT NULL,
+  `status` varchar(255) DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `trainer_id` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`requirement_id`),
+  KEY `FKqndc0ptamgtq4horxxrrieo7n` (`trainer_id`),
+  CONSTRAINT `FKqndc0ptamgtq4horxxrrieo7n` FOREIGN KEY (`trainer_id`) REFERENCES `trainer` (`trainer_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `requirement`
+--
+
+LOCK TABLES `requirement` WRITE;
+/*!40000 ALTER TABLE `requirement` DISABLE KEYS */;
+/*!40000 ALTER TABLE `requirement` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `trainer`
+--
+
+DROP TABLE IF EXISTS `trainer`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `trainer` (
+  `trainer_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `certification` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `experience` varchar(255) DEFAULT NULL,
+  `expertise` varchar(255) DEFAULT NULL,
+  `joining_date` date DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `phone` varchar(255) DEFAULT NULL,
+  `resume` longblob,
+  `status` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`trainer_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `trainer`
+--
+
+LOCK TABLES `trainer` WRITE;
+/*!40000 ALTER TABLE `trainer` DISABLE KEYS */;
+/*!40000 ALTER TABLE `trainer` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -69,7 +139,7 @@ CREATE TABLE `user` (
   `user_role` varchar(255) DEFAULT NULL,
   `username` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -78,7 +148,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'admin@gmail.com','1234567890','$2a$10$NxtLTA8aNSQJqm/8PNM8j.jUlo0Kjkk7AIB8m621F8zbs04duk16a','Admin','admin'),(2,'demouser@gmail.com','9876543211','$2a$10$gL1.6XH.ptx7W6nLoSmqBeVZ43oQhjZPsoy/mpLk3M7.lu5xpG.r2','User','demouser');
+INSERT INTO `user` VALUES (1,'admin@gmail.com','1234567890','$2a$10$NxtLTA8aNSQJqm/8PNM8j.jUlo0Kjkk7AIB8m621F8zbs04duk16a','Admin','admin'),(2,'demouser@gmail.com','9876543211','$2a$10$gL1.6XH.ptx7W6nLoSmqBeVZ43oQhjZPsoy/mpLk3M7.lu5xpG.r2','User','demouser'),(3,'manager@gmail.com','1234567890','$2a$10$8carrOcJfRnf9YX/7g3kcuubXf4UyIGEvlnG4zeIQPUU.LUXH5X.a','manager','Manager');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -161,4 +231,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-12-12  8:32:48
+-- Dump completed on 2024-12-12  8:47:48
