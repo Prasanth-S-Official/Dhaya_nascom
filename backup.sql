e253a0543e91ff07a18f0f5e69bfaa922c1c211b
neojarvis <techadmin@iamneo.ai>
2024-12-17 08:30:15 +0000

    Auto-save-commit-17/12/2024--14:00:14

--- backup.sql
+++ backup.sql
@@ -14,3 +14,153 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Current Database: `appdb`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `appdb` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `appdb`;
+
+--
+-- Table structure for table `feedback`
+--
+
+DROP TABLE IF EXISTS `feedback`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `feedback` (
+  `feedback_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `category` varchar(255) DEFAULT NULL,
+  `date` date DEFAULT NULL,
+  `feedback_text` varchar(255) DEFAULT NULL,
+  `rating` int(11) DEFAULT NULL,
+  `agent_id` bigint(20) DEFAULT NULL,
+  `ticket_id` bigint(20) NOT NULL,
+  `user_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`feedback_id`),
+  KEY `FKss3q5bmq5ojxbu3rc3sk4ckuy` (`agent_id`),
+  KEY `FK8fth5acovhce06e6atsujlb6e` (`ticket_id`),
+  KEY `FK7k33yw505d347mw3avr93akao` (`user_id`),
+  CONSTRAINT `FK7k33yw505d347mw3avr93akao` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`),
+  CONSTRAINT `FK8fth5acovhce06e6atsujlb6e` FOREIGN KEY (`ticket_id`) REFERENCES `ticket` (`ticket_id`),
+  CONSTRAINT `FKss3q5bmq5ojxbu3rc3sk4ckuy` FOREIGN KEY (`agent_id`) REFERENCES `support_agent` (`agent_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `feedback`
+--
+
+LOCK TABLES `feedback` WRITE;
+/*!40000 ALTER TABLE `feedback` DISABLE KEYS */;
+/*!40000 ALTER TABLE `feedback` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `support_agent`
+--
+
+DROP TABLE IF EXISTS `support_agent`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `support_agent` (
+  `agent_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `added_date` date DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `experience` varchar(255) DEFAULT NULL,
+  `expertise` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `phone` varchar(255) DEFAULT NULL,
+  `profile` longblob,
+  `remarks` varchar(255) DEFAULT NULL,
+  `shift_timing` varchar(255) DEFAULT NULL,
+  `status` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`agent_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `support_agent`
+--
+
+LOCK TABLES `support_agent` WRITE;
+/*!40000 ALTER TABLE `support_agent` DISABLE KEYS */;
+/*!40000 ALTER TABLE `support_agent` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `ticket`
+--
+
+DROP TABLE IF EXISTS `ticket`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `ticket` (
+  `ticket_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `created_date` date DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `issue_category` varchar(255) DEFAULT NULL,
+  `priority` varchar(255) DEFAULT NULL,
+  `resolution_date` date DEFAULT NULL,
+  `resolution_summary` varchar(255) DEFAULT NULL,
+  `status` varchar(255) DEFAULT NULL,
+  `title` varchar(255) DEFAULT NULL,
+  `agent_id` bigint(20) DEFAULT NULL,
+  `user_id` bigint(20) NOT NULL,
+  PRIMARY KEY (`ticket_id`),
+  KEY `FKc2ajxrr4s4whuncj5xpab9by1` (`agent_id`),
+  KEY `FKdvt57mcco3ogsosi97odw563o` (`user_id`),
+  CONSTRAINT `FKc2ajxrr4s4whuncj5xpab9by1` FOREIGN KEY (`agent_id`) REFERENCES `support_agent` (`agent_id`),
+  CONSTRAINT `FKdvt57mcco3ogsosi97odw563o` FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ticket`
+--
+
+LOCK TABLES `ticket` WRITE;
+/*!40000 ALTER TABLE `ticket` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ticket` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `user_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) DEFAULT NULL,
+  `mobile_number` varchar(255) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `user_role` varchar(255) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'client@gmail.com','1234567890','$2a$10$q9YeAjztDwL9kNVbJs2u3e0iQOdLNbInf3lCyIhnkoY/Y2CiidCuy','Client','Client');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-12-17  8:30:15
