3f2deb8a64d3be7fc38dee921eca26102b2382fa
neojarvis <techadmin@iamneo.ai>
2024-12-30 05:35:49 +0000

    Auto-save-commit-30/12/2024--11:05:46

--- ClientSupport/angularapp/src/app/services/support-agent.service.spec.ts
+++ ClientSupport/angularapp/src/app/services/support-agent.service.spec.ts
@@ -1,16 +1,19 @@
 import { TestBed } from '@angular/core/testing';
 
 import { SupportAgentService } from './support-agent.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('SupportAgentService', () => {
   let service: SupportAgentService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+    });
     service = TestBed.inject(SupportAgentService);
   });
 
-  it('should be created', () => {
+  fit('Frontend_should_support_agent_service', () => {
     expect(service).toBeTruthy();
   });
 });
