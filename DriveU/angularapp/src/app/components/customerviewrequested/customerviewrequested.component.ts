9da51de5bab376b0d8130da0f96e59c31731bd6b
neojarvis <techadmin@iamneo.ai>
2025-01-05 15:17:03 +0000

    Auto-save-commit-05/01/2025--20:47:01

--- DriveU/angularapp/src/app/components/customerviewrequested/customerviewrequested.component.ts
+++ DriveU/angularapp/src/app/components/customerviewrequested/customerviewrequested.component.ts
@@ -1,89 +1,3 @@
-// import { Component, OnInit } from '@angular/core';
-// import { Router } from '@angular/router';
-// import { DriverRequestService } from 'src/app/services/driver-request.service';
-
-// @Component({
-//   selector: 'app-customerviewrequested',
-//   templateUrl: './customerviewrequested.component.html',
-//   styleUrls: ['./customerviewrequested.component.css'],
-// })
-// export class CustomerviewrequestedComponent implements OnInit {
-//   showDeletePopup = false;
-//   showDetailsModal = false;
-//   requestToDelete: any = null;
-//   selectedRequest: any = null;
-//   appliedRequests: any[] = [];
-//   filteredRequests: any[] = [];
-//   searchValue = '';
-
-//   constructor(private driverRequestService: DriverRequestService, private router: Router) {}
-
-//   ngOnInit(): void {
-//     this.fetchData();
-//   }
-
-//   fetchData(): void {
-//     const userId = Number(localStorage.getItem('userId'));
-//     this.driverRequestService.getDriverRequestsByUserId(userId).subscribe(
-//       (response: any) => {
-//         console.log('User Requested Drivers:', response);
-//         this.appliedRequests = response;
-//         this.filteredRequests = response;
-//       },
-//       (error) => {
-//         console.error('Error fetching data:', error);
-//       }
-//     );
-//   }
-
-//   filterRequests(): void {
-//     const searchLower = this.searchValue.toLowerCase();
-//     this.filteredRequests = this.appliedRequests.filter(request =>
-//       request.driver.driverName.toLowerCase().includes(searchLower)
-//     );
-//   }
-
-//   handleDeleteClick(request: any): void {
-//     this.requestToDelete = request;
-//     this.showDeletePopup = true;
-//   }
-
-//   handleConfirmDelete(): void {
-//     this.driverRequestService.deleteDriverRequest(this.requestToDelete.driverRequestId).subscribe(
-//       () => {
-//         // Remove the deleted request from the arrays
-//         this.appliedRequests = this.appliedRequests.filter(
-//           (req) => req.driverRequestId !== this.requestToDelete.driverRequestId
-//         );
-//         this.filteredRequests = [...this.appliedRequests]; // Update the filtered list
-
-//         this.closeDeletePopup(); // Close the popup
-//       },
-//       (error) => {
-//         console.error('Error deleting request:', error);
-//       }
-//     );
-//   }
-
-//   closeDeletePopup(): void {
-//     this.requestToDelete = null;
-//     this.showDeletePopup = false;
-//   }
-
-//   showRequestDetails(request: any): void {
-//     this.selectedRequest = request;
-//     this.showDetailsModal = true;
-//   }
-
-//   closeDetailsModal(): void {
-//     this.selectedRequest = null;
-//     this.showDetailsModal = false;
-//   }
-
-//   navigateToEdit(id: number): void {
-//     this.router.navigate(['/customer/edit/request', id]);
-//   }
-// }
 
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
@@ -113,6 +27,8 @@ export class CustomerviewrequestedComponent implements OnInit {
     const userId = Number(localStorage.getItem('userId'));
     this.driverRequestService.getDriverRequestsByUserId(userId).subscribe(
       (response: any) => {
+        console.log(response);
+        
         this.appliedRequests = response;
         this.filteredRequests = response;
       },
