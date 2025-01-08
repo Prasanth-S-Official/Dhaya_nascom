9905fa0632f0a1918080735c0303bdaf5571a9e4
neojarvis <techadmin@iamneo.ai>
2025-01-08 10:25:22 +0000

    Auto-save-commit-08/01/2025--15:55:20

---
+++ ProBid/angularapp/src/app/components/clientnav/clientnav.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-clientnav',
+  templateUrl: './clientnav.component.html',
+  styleUrls: ['./clientnav.component.css']
+})
+export class ClientnavComponent implements OnInit {
+  showLogoutPopup = false;
+  userName = '';
+  userRole = '';
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    const storedUserName = localStorage.getItem('username');
+    const storedUserRole = localStorage.getItem('userRole');
+
+    this.userName = storedUserName || '';
+    this.userRole = storedUserRole || '';
+  }
+
+  logout(): void {
+    localStorage.clear();
+    this.router.navigate(['/login']);
+  }
+}
