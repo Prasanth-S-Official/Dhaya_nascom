783cadada4e3d8cb36dbea0f79bc7bc1ff38410a
neojarvis <techadmin@iamneo.ai>
2025-01-23 11:34:56 +0000

    Auto-save-commit-23/01/2025--17:04:55

--- backup.sql
+++ backup.sql
@@ -125,7 +125,7 @@ CREATE TABLE `ticket` (
 
 LOCK TABLES `ticket` WRITE;
 /*!40000 ALTER TABLE `ticket` DISABLE KEYS */;
-INSERT INTO `ticket` VALUES (1,'2025-01-23','demo connectivity issue','Connectivity','Low',NULL,NULL,NULL,'Open','demo connectivity',NULL,1),(2,'2025-01-23','demo start issue description','Platform Bug','High',NULL,NULL,NULL,'Open','demo platform start issue',3,1),(3,'2025-01-23','demo content issue description','Content Issue','Medium','2025-01-23','demo resolution summary on content issue.','','Resolved','demo content issue',4,1),(4,'2025-01-23','demo dotnet test issue','Tech Stacks','High',NULL,NULL,NULL,'Open','dotnet test',NULL,1),(5,'2025-01-23','demo issue on provided content mismatch','Content Issue','Medium',NULL,NULL,NULL,'Open','demo content question issue',NULL,1);
+INSERT INTO `ticket` VALUES (1,'2025-01-23','demo connectivity issue','Connectivity','Low',NULL,NULL,NULL,'Open','demo connectivity',NULL,1),(2,'2025-01-23','demo start issue description','Platform Bug','High','2025-01-23','demo resolution on platform issue','\0','Resolved','demo platform start issue',3,1),(3,'2025-01-23','demo content issue description','Content Issue','Medium','2025-01-23','demo resolution summary on content issue.','','Closed','demo content issue',4,1),(4,'2025-01-23','demo dotnet test issue','Tech Stacks','High',NULL,NULL,NULL,'Open','dotnet test',NULL,1),(5,'2025-01-23','demo issue on provided content mismatch','Content Issue','Medium',NULL,NULL,NULL,'Open','demo content question issue',NULL,1);
 /*!40000 ALTER TABLE `ticket` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -166,4 +166,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-01-23 11:19:59
+-- Dump completed on 2025-01-23 11:34:56
