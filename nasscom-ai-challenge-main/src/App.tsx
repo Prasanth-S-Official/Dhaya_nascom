432a1feb0d560ae3827c77ecee211ea56a41afdd
neojarvis <techadmin@iamneo.ai>
2025-04-06 06:19:38 +0000

    Auto-save-commit-06/04/2025--11:49:37

--- nasscom-ai-challenge-main/src/App.tsx
+++ nasscom-ai-challenge-main/src/App.tsx
@@ -3,7 +3,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate, Outlet } from "react-router-dom";
 import { FormProvider } from "./context/FormContext";
 
 // Import pages
@@ -14,6 +14,7 @@ import FormPage3 from "./pages/FormPage3";
 import ReviewPage from "./pages/ReviewPage";
 import SuccessPage from "./pages/SuccessPage";
 import NotFound from "./pages/NotFound";
+import { FormLayout } from "./components/FormLayout";
 
 const queryClient = new QueryClient();
 
@@ -24,18 +25,32 @@ const App = () => (
       <Sonner />
       <FormProvider>
         <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Index />} />
-            <Route path="/form">
-              <Route index element={<Navigate to="/form/page1" replace />} />
-              <Route path="page1" element={<FormPage1 />} />
-              <Route path="page2" element={<FormPage2 />} />
-              <Route path="page3" element={<FormPage3 />} />
-              <Route path="review" element={<ReviewPage />} />
-              <Route path="success" element={<SuccessPage />} />
-            </Route>
-            <Route path="*" element={<NotFound />} />
-          </Routes>
+        <Routes>
+        <Route
+            path="/"
+            element={
+              <FormLayout
+                onSave={() => console.log("Save clicked")}
+                onNext={() => console.log("Next clicked")}
+                currentStep={1}
+                totalSteps={4}
+                isLastStep={false}
+              >
+                <Outlet /> {/* This renders children routes inside FormLayout */}
+              </FormLayout>
+            }
+          >
+            <Route index element={<Navigate to="form/page1" replace />} />
+            <Route path="form/page1" element={<FormPage1 />} />
+            <Route path="form/page2" element={<FormPage2 />} />
+            <Route path="form/page3" element={<FormPage3 />} />
+            <Route path="form/review" element={<ReviewPage />} />
+            <Route path="form/success" element={<SuccessPage />} />
+          </Route>
+
+<Route path="*" element={<NotFound />} />
+</Routes>
+
         </BrowserRouter>
       </FormProvider>
     </TooltipProvider>
